d7ffaa312f9e325300de8feffa4af9c230e1965e
unknown <Samuca23>
2023-12-28 13:38:33 -0300

    delete

--- api/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -26,7 +26,7 @@ public class MedicoController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable) {
-        return repository.findAll(pageable).map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(pageable).map(DadosListagemMedico::new);
     }
 
     @PutMapping
@@ -34,5 +34,18 @@ public class MedicoController {
     public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+    @DeleteMapping("/deleteLogic/{id}")
+    @Transactional
+    public void excluirLogica(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluirLogic();
     }
 }

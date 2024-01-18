a6abbd420acc4bd2e63a4298bf79644413ffb6d0
Samuel <samuel.chiodini@unidavi.edu.br>
2024-01-18 18:16:43 -0300

    up

---
+++ api/api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -0,0 +1,15 @@
+package med.voll.api.infra.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    public SecurityFilterChain securityFilterChain() {
+
+    }
+
+}

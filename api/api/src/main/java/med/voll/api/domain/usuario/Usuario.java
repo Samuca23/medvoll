5db6b34851c137baa9e562a8a0d3ce406718ced1
Samuel <samuel.chiodini@unidavi.edu.br>
2024-01-18 15:39:46 -0300

    entidade JPA de usuarioas e migration usuarios

---
+++ api/api/src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,24 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    private String senha;
+
+}
